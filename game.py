a72b5705aa1e09716306a8d0399b35a27d8d2166
plton228 <platontontonton@gmial.com>
2025-12-04 17:53:52 +0200

    add class Participant

--- game.py
+++ game.py
@@ -14,3 +14,20 @@ class Lot:
         else:
             print("Bid is lower than starting price.")
             return False
+
+class Participant:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def __repr__(self):
+        return f"Participant(name={self.name}, balance={self.balance})"
+    
+    def place_bid(self, lot, bid_amount):
+        if bid_amount > self.balance:
+            print("Insufficient balance to place the bid.")
+            return False
+        if lot.update_leader(self.name, bid_amount):
+            self.balance -= bid_amount
+            return True
+        return False
